68958265ba4de52b40a5a26a18078572a20da940
Natnael Worku <natnaelworku18@gmail.com>
2026-09-03 11:14:15 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+class Settings:
+    OPEN_METEO_BASE_URL: str = "https://api.open-meteo.com/v1/forecast"
+    OPEN_METEO_SEASONAL_URL: str = "https://climate-api.open-meteo.com/v1/climate"
+
+settings = Settings()
